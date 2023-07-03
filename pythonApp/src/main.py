49441227428d7b235e83229bf290dca936419a97
Arun K Singh <singh.arunk16@hotmail.com>
2023-07-03 19:31:44 +0100

    Create main.py

---
+++ pythonApp/src/main.py
@@ -0,0 +1,39 @@
+import pymysql
+from app import app
+from config import mysql
+from flask import jsonify
+from flask import flash, request, Flask
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify(message='Hello, World!')
+
+@app.route('/emp')
+def emp():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT PersonID, LastName, FirstName, Address, City FROM Persons")
+        empRows = cursor.fetchall()
+        respone = jsonify(empRows)
+        respone.status_code = 200
+        return respone
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.errorhandler(404)
+def showMessage(error=None):
+    message = {
+        'status': 404,
+        'message': 'Record not found: ' + request.url,
+    }
+    respone = jsonify(message)
+    respone.status_code = 404
+    return respone
+
+if __name__ == "__main__":
+    app.run()
